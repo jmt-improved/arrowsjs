51f846f11e188e9489175a01d544842779f2e759
Raffaele Fioratto <raffaele.fioratto@gmail.com>
2016-11-26 16:19:30 +0100

    Take into account start and end point of lines
    Signed-off-by: Raffaele Fioratto <raffaele.fioratto@gmail.com>

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
-dim = 40
-var matrix = [[0, -1, 1, 1, 2, 2, -1, 0],
-              [0,  0, 0, 1, 2, 0,  0, 0],
-              [0,  0, 2, [1, 2], 2, 0,  0, 0],
-              [0,  0, 2, 1, [1,3], [1, 3], -1, 0],
-              [0, -1, [2,3], 3, 3, 0, 0, 0]];
+// Warn if overriding existing method
+if(Array.prototype.equals)
+    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
 
-function drawArrows(cx, m, sq_dim) {
+    // compare lengths - can save a lot of time 
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;       
+        }           
+        else if (this[i] != array[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+
+dim = 40
+
+var lines = [
+    [[0,2],[2,0]],
+    [[2,3],[3,0]]
+];
+
+var matrixDemo = [
+    [-1,-1,[1],-1],
+    [-1,-1,[1],-1],
+    [[1],[1],[1],[2]],
+    [[2],[2],[2],[2]],
+];
+
+var matrixDemo2 = [
+    [-1,-1,[1],-1],
+    [-1,-1,[1],-1],
+    [[1],[1],[1,2],[2]],
+    [[2],[2],[2],0],
+];
+
+var matrixDemo3 = [
+    [-1,-1,[1],-1],
+    [-1,-1,[1],-1],
+    [[1],[1],[1,2],[2]],
+    [[2],[2],[2],[2]],
+];
+
+function drawArrows(cx, m, l, sq_dim) {
   "use strict";
   var neighbors = [[-1, 0], // TOP
                    [0, -1], // LEFT
@@ -31,9 +82,10 @@ function drawArrows(cx, m, sq_dim) {
               ii = i + neighbors[k][0];
               jj = j + neighbors[k][1];
               if (m[ii] !== undefined && m[ii][jj] !== undefined) {
-                if (m[ii][jj] == -1 || m[ii][jj] == m[i][j][t] || 
+                if (((m[ii][jj] == -1) && (l[m[i][j][t] - 1][0].equals([i, j])) || 
+                    l[m[i][j][t] - 1][1].equals([i, j])) || 
                    (typeof(m[ii][jj]) == "object" && 
-                    (m[ii][jj].indexOf(m[i][j][t]) != -1))) {m
+                    (m[ii][jj].indexOf(m[i][j][t]) != -1))) {
                   if (!moved) {
                     tmp_x = j * sq_dim + n_formula[k][0]
                     tmp_y = i * sq_dim + n_formula[k][1]
@@ -62,48 +114,12 @@ function drawArrows(cx, m, sq_dim) {
           }
         }
       }
-      else if (m[i][j] > 0) {
-        moved = false;
-        cx.beginPath();
-        for (k = 0; k < 4; k++) {
-          ii = i + neighbors[k][0];
-          jj = j + neighbors[k][1];
-          if (m[ii] !== undefined && m[ii][jj] !== undefined) {
-            if (m[ii][jj] == -1 || m[ii][jj] == matrix[i][j] || 
-               (typeof(m[ii][jj]) == "object" && (m[ii][jj].indexOf(m[i][j]) != -1))) {
-              if (!moved) {
-                tmp_x = j * sq_dim + n_formula[k][0]
-                tmp_y = i * sq_dim + n_formula[k][1]
-                cx.moveTo(tmp_x , tmp_y);
-                moved = true;
-              } else {
-                tmp_xx = j * sq_dim + n_formula[k][0]
-                tmp_yy = i * sq_dim + n_formula[k][1]
-                if ((tmp_x !== tmp_xx) && (tmp_y !== tmp_yy)) {
-                  if (k == 1) {
-                    cx.lineTo(tmp_x, tmp_yy);      
-                  }
-                  else if (k == 2) {
-                    cx.lineTo(tmp_x, tmp_yy);
-                  }
-                  else if (k == 3) {
-                    cx.lineTo(tmp_xx, tmp_y);
-                  }
-                }
-                cx.lineTo(tmp_xx, tmp_yy);
-              }
-            }
-          }
-        }
-        cx.stroke();
-      }
     }
   }
 }
-function test() {
-	"use strict";	
-	var ctx = document.getElementById("myCanvas").getContext("2d");
-	ctx.lineWidth = 5;
-	ctx.strokeStyle = 'black';
-	drawArrows(ctx, matrix, dim)
-}
+
+var ctx = document.getElementById("myCanvas").getContext("2d");
+ctx.lineWidth = 1;
+ctx.strokeStyle = 'black';
+drawArrows(ctx, matrixDemo, lines, dim)
+
